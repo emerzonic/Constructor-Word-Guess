f1b64082d299138c434c7726030c5d9793bf97de
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-14 10:08:59 -0500

    change index logic to a js class

--- index.js
+++ index.js
@@ -1,38 +1,61 @@
-var Word = require('./Word');
-var wordsBank = require('./wordsbank');
-var inquirer = require("inquirer");
-var newWord;
+class GameController {
+    constructor() {
+        this.newWord = '';
+        this.score = 0;
+    }
 
-//generate randon word from wordsBank.js
-var generateWord = function () {
-    randomWord = wordsBank[Math.floor(Math.random() * wordsBank.length)];
-    console.log('YOU GOT A NEW WORD!');
-    //console.log(randomWord); //for testing only
-    newWord = new Word(randomWord);
-    //split the letters of the object word and display placeholders to user
-    newWord
-        .splitLetters()
-        .displayWord();
-    takeUserGuess();
-};
+    // generate randon word from wordsBank.js
+    generateWord() {
+        const wordsBank = require('./wordsbank');
+        const Word = require('./Word');
+        const randomWord = wordsBank[Math.floor(Math.random() * wordsBank.length)];
+        console.log('YOU GOT A NEW WORD!');
+        console.log(randomWord); //for testing only
+        this.newWord = new Word(randomWord);
+        this.newWord
+            .splitLetters()
+            .displayWord();
+        this.takeUserGuess();
+    }
 
-generateWord();
+    //Checks the word's status, prompts and takes the user guess and validate
+    takeUserGuess() {
+        if (this.newWord.status) {
+            this.score++;
+            console.log(`Your score is: ${this.score}`);
+            this.resetGame();
+        } else {
+            const inquirer = require("inquirer");
+            inquirer.prompt([{
+                name: "guess",
+                message: "Guess a letter."
+            }]).then(letter => {
+                //Check user guess, track word status and display word to user
+                this.newWord
+                    .takeChar(letter.guess)
+                    .trackStatus()
+                    .displayWord();
+                this.takeUserGuess();
+            });
+        }
+    }
 
-//Checks the word's status, prompts and takes the user guess and validate
-function takeUserGuess() {
-    if (newWord.status) {
-        generateWord();
-    } else {
-        inquirer.prompt([{
-            name: "guess",
-            message: "Guess a letter."
-        }]).then(letter => {
-            //Check user guess, track word status and display word to user
-            newWord
-                .takeChar(letter.guess)
-                .trackStatus()
-                .displayWord();
-            takeUserGuess();
-        });
+    //Ask the user if they want to continue playing or not
+    resetGame(){
+        const confirm = require('inquirer-confirm');
+        confirm('WOULD YOU LIKE TO PLAY AGAIN?')
+          .then(() => {
+            //if yes, generate new word
+            this.generateWord();
+          }, function cancelled() {
+            console.log('THANKS FOR PLAYING!');
+            return;
+          });
     }
 }
+
+
+
+//start a new game
+const game = new GameController();
+game.generateWord();
