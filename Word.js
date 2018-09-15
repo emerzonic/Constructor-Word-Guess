27f33aa3c2b6dabbbea91aba267efa88b9927f7d
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-15 12:22:33 -0500

    add fail attempts feature

--- Word.js
+++ Word.js
@@ -7,7 +7,9 @@ class Word {
         this.splittedLetters = [];
         this.status = false;
         this.feedback = 0;
+        this.attempts = 0;
     }
+
     //This method takes a word and splits the letters into objects
     splitLetters() {
         let arr = this.word.split("");
@@ -18,21 +20,10 @@ class Word {
         return this;
     }
 
-    //This method tracks the status of the guesses remaining and updates if guess is wrong or correct
-    trackStatus() {
-        let track = 0;
-        this.splittedLetters.forEach(obj => {
-            if (obj.checkGuess() !== '_') {
-                track++;
-            }
-        });
-        if (this.feedback !== track) {
-            console.log('\x1b[32m', 'CORRECT!');
-            this.feedback = track;
-        } else {
-            console.log('\x1b[31m','INCORRECT!');
-        }
-        console.log(`You have ${Number(this.word.length - track)} more remaining`);
+    //This method generates the number of attempts base on the length of the random word
+    generateAttempts(){
+        this.attempts = this.word.length * 3;
+        console.log('\x1b[31m',`You have ${this.attempts} fail attempts to make on this word.`);
         return this;
     }
 
@@ -56,6 +47,27 @@ class Word {
         this.splittedLetters.forEach(obj => {
             obj.takeGuess(guess);
         });
+        return this;
+    }
+
+    //This method tracks the status of the guesses remaining and updates if guess is wrong or correct
+    trackStatus() {
+        let track = 0;
+        this.splittedLetters.forEach(obj => {
+            if (obj.checkGuess() !== '_') {
+                track++;
+            }
+        });
+        if (this.feedback !== track) {
+            console.log('\x1b[32m', 'CORRECT!');
+            this.feedback = track;
+        } else {
+            this.attempts--;
+            console.log('\x1b[31m', 'INCORRECT!');
+            console.log('\x1b[31m',`You have ${this.attempts} ${this.attempts <= 1?'attempt':'attempts'} remaining.`);
+        }
+        let remaining = this.word.length - track;
+        console.log(`...${Number(remaining)} more ${remaining <= 1?'letter':'letters'} to guess it right.`);
         return this;
     }
 }
